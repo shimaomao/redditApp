e455c96f04bcb4286ec518e36b4234e22b88cffd
Christopher Cardenas <chris.cardenas93@gmail.com>
2017-06-18 10:02:46 -0500

    Add ObjectNode and additional changes to RedditService

--- src/main/java/com/cardechr/reddit/controller/RedditController.java
+++ src/main/java/com/cardechr/reddit/controller/RedditController.java
@@ -1,17 +1,16 @@
 package com.cardechr.reddit.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cardechr.reddit.domain.Subreddit;
 import com.cardechr.reddit.service.RedditService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -26,33 +25,33 @@ public class RedditController {
     this.redditService = redditService;
   }
 
-  @RequestMapping(value ="/", method = RequestMethod.GET ,produces = "application/json; charset=UTF-8")
-  public List<Subreddit> getSubreddit() throws InterruptedException, ExecutionException {
+  @RequestMapping(value ="/future", method = RequestMethod.GET ,produces = "application/json; charset=UTF-8")
+  public List<String> getSubreddit() throws InterruptedException, ExecutionException, JsonProcessingException, IOException {
     
-    List<Subreddit> list = new ArrayList<>();
-    CompletableFuture<Subreddit> r1 = redditService.getSubreddit(SUBREDDITS[0]);
-    CompletableFuture<Subreddit> r2 = redditService.getSubreddit(SUBREDDITS[1]);
-    CompletableFuture<Subreddit> r3 = redditService.getSubreddit(SUBREDDITS[2]);
-    CompletableFuture<Subreddit> r4 = redditService.getSubreddit(SUBREDDITS[3]);
-    CompletableFuture<Subreddit> r5 = redditService.getSubreddit(SUBREDDITS[4]);
+    List<String> list = new ArrayList<>();
+    CompletableFuture<String> r1 = redditService.getSubreddit(SUBREDDITS[0]);
+    CompletableFuture<String> r2 = redditService.getSubreddit(SUBREDDITS[1]);
+    CompletableFuture<String> r3 = redditService.getSubreddit(SUBREDDITS[2]);
+    CompletableFuture<String> r4 = redditService.getSubreddit(SUBREDDITS[3]);
+    CompletableFuture<String> r5 = redditService.getSubreddit(SUBREDDITS[4]);
     CompletableFuture.allOf(r1,r2,r3,r4,r5).join();
 
     list.add(r1.get());
     list.add(r2.get());
     list.add(r3.get());
     list.add(r4.get());
-    list.add(r5.get());
+    list.add(r5.get());	
 
     return list;
   }
   
-  @RequestMapping(value = "/top", method = RequestMethod.GET, headers = "Accept=application/json")
-  public List<String> getPost() throws JsonProcessingException, InterruptedException, ExecutionException {
+  @RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
+  public List<String> getPost() throws InterruptedException, ExecutionException, IOException {
     return redditService.getTopPosts();
   }
   
   @RequestMapping(value = "/singlePost", method = RequestMethod.GET, headers = "Accept=application/json")
-  public ResponseEntity<String> getAPost() {
+  public String getAPost() throws IOException {
     return redditService.getPost(SUBREDDITS[1]);
   }
 }
--- src/main/java/com/cardechr/reddit/service/RedditService.java
+++ src/main/java/com/cardechr/reddit/service/RedditService.java
@@ -1,5 +1,6 @@
 package com.cardechr.reddit.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -16,69 +17,79 @@ import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
-import com.cardechr.reddit.domain.Subreddit;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Service
 public class RedditService {
 
-  private final RestTemplate restTemplate;
-  
-  private String[] urls = { "https://reddit.com/r/IAmA/top.json?limit=1",
-    "https://reddit.com/r/programming/top.json?limit=1", "https://reddit.com/r/mildlyinteresting/top.json?limit=1",
-    "https://reddit.com/r/AskReddit/top.json?limit=1", "https://reddit.com/r/tastyfood/top.json?limit=1" };
-  
-  public RedditService(RestTemplateBuilder restTemplateBuilder) {
-    this.restTemplate = restTemplateBuilder.build();
-  }
-  
-  // AsyncRestTemplate / ListenableFuture
-  
-  @Async
-  public CompletableFuture<Subreddit> getSubreddit(String sub) {
-    
-    String url = String.format("https://reddit.com/r/%s/top.json?limit=1", sub);
-    System.out.println(url);
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
-    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+	private ObjectNodeService ons;
+	private final RestTemplate restTemplate;
 
-    ResponseEntity<Subreddit> results = restTemplate.exchange(url, HttpMethod.GET, entity, Subreddit.class);
-    return CompletableFuture.completedFuture(results.getBody());
-  }
-  
-  public ResponseEntity<String> getPost(String sub) {
-    String url = String.format("https://reddit.com/r/%s/top.json?limit=1", sub);
-    RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
-    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-    ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-    System.out.println(result);
-    return result;
-  }
-  
-  public List<String> getTopPosts() throws JsonProcessingException, InterruptedException, ExecutionException {
+	private String[] urls = { "https://reddit.com/r/IAmA/top.json?limit=1",
+			"https://reddit.com/r/programming/top.json?limit=1",
+			"https://reddit.com/r/mildlyinteresting/top.json?limit=1",
+			"https://reddit.com/r/AskReddit/top.json?limit=1", "https://reddit.com/r/tastyfood/top.json?limit=1" };
 
-    AsyncRestTemplate restTemplate = new AsyncRestTemplate();
+	public RedditService(RestTemplateBuilder restTemplateBuilder, ObjectNodeService ons) {
+		this.restTemplate = restTemplateBuilder.build();
+		this.ons = ons;
+	}
 
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
-    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-    
-    List<ListenableFuture<ResponseEntity<String>>> futures = new ArrayList<>();
-    for (String url : urls) {
-      ListenableFuture<ResponseEntity<String>> responseEntityFuture = restTemplate.exchange(url, HttpMethod.GET,
-        entity, String.class);
-      
-      futures.add(responseEntityFuture);
-      }
 
-    //List<ResponseEntity<String>> response = new ArrayList<>();
-    List<String> body = new ArrayList<>();
-    for (ListenableFuture<ResponseEntity<String>> future : futures) { 
-      body.add(future.get().getBody());
-    } 
-    return body;
-  }
+	@Async
+	public CompletableFuture<String> getSubreddit(String sub) throws JsonProcessingException, IOException {
+		
+		String url = String.format("https://reddit.com/r/%s/top.json?limit=1", sub);
+		System.out.println(url);
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+		ResponseEntity<String> results = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+		String r = results.getBody();
+		String response = ons.getJsonBody(r.toString());
+		return CompletableFuture.completedFuture(response);
+	}
+
+	public String getPost(String sub) throws IOException {
+		
+		String url = String.format("https://reddit.com/r/%s/top.json?limit=1", sub);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+		
+		String resultBody = result.getBody();
+		String response = ons.getJsonBody(resultBody);
+
+		return response;
+	}
+
+	// AsyncRestTemplate / ListenableFuture
+	public List<String> getTopPosts() throws IOException, InterruptedException, ExecutionException {
+
+		AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+		List<ListenableFuture<ResponseEntity<String>>> futures = new ArrayList<>();
+		for (String url : urls) {
+			ListenableFuture<ResponseEntity<String>> responseEntityFuture = asyncRestTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+			futures.add(responseEntityFuture);
+		}
+
+		//List<ResponseEntity<String>> response = new ArrayList<>();
+		List<String> response = new ArrayList<>();
+		for (ListenableFuture<ResponseEntity<String>> future : futures) {		
+			String pretty = ons.getJsonBody(future.get().getBody());	
+			response.add(pretty);
+		}
+		
+		return response;
+	}
 }
