462b131d712ae2b4ea35be693f07a7e016fe0430
Christopher Cardenas <chris.cardenas93@gmail.com>
2017-06-20 07:06:01 -0500

    Remove unnecessary variables & Move ObjectNodeService logic to RedditService

--- src/main/java/com/cardechr/reddit/service/RedditService.java
+++ src/main/java/com/cardechr/reddit/service/RedditService.java
@@ -17,22 +17,25 @@ import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
+import com.cardechr.reddit.domain.Data;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class RedditService {
 
-	private ObjectNodeService ons;
 	private final RestTemplate restTemplate;
+  private ObjectMapper mapper;
 
 	private String[] urls = { "https://reddit.com/r/IAmA/top.json?limit=1",
 			"https://reddit.com/r/programming/top.json?limit=1",
 			"https://reddit.com/r/mildlyinteresting/top.json?limit=1",
 			"https://reddit.com/r/AskReddit/top.json?limit=1", "https://reddit.com/r/tastyfood/top.json?limit=1" };
 
-	public RedditService(RestTemplateBuilder restTemplateBuilder, ObjectNodeService ons) {
+	public RedditService(RestTemplateBuilder restTemplateBuilder) {
 		this.restTemplate = restTemplateBuilder.build();
-		this.ons = ons;
 	}
 
 	@Async
@@ -42,18 +45,17 @@ public class RedditService {
 		HttpEntity<String> entity = setUserAgent();
 		ResponseEntity<String> results = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-		return CompletableFuture.completedFuture(ons.getJsonBody(results.getBody().toString()));
+		return CompletableFuture.completedFuture(getJsonBody(results.getBody().toString()));
 	}
 
 	public String getPost(String sub) throws IOException {
-		
+	  
 		String url = String.format("https://reddit.com/r/%s/top.json?limit=1", sub);
-		
 		RestTemplate restTemplate = new RestTemplate();
 		HttpEntity<String> entity = setUserAgent();
 		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 		
-		return ons.getJsonBody(result.getBody());
+		return getJsonBody(result.getBody());
 	}
 
 	// AsyncRestTemplate / ListenableFuture
@@ -70,16 +72,32 @@ public class RedditService {
 
 		List<String> response = new ArrayList<>();
 		for (ListenableFuture<ResponseEntity<String>> future : futures) {
-			response.add(ons.getJsonBody(future.get().getBody()));
+		  response.add(getJsonBody(future.get().getBody()));
 		}
 		return response;
 	}
 	
-  // Reddit API recommends JAVA Apps to have custom User-Agent else it response with 429 (Too Many Requests)
+  // Reddit API recommends JAVA Apps to have custom User-Agent else it responds with 429 (Too Many Requests)
 	private HttpEntity<String> setUserAgent(){
 	  HttpHeaders headers = new HttpHeaders();
     headers.set("User-Agent", "other:com.cardechr.reddit:v1.0 (by /u/redprog12)");
     HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 	  return entity;
 	}
+	
+	// Parses requset using Jackson to get elements in Data Class
+	private String getJsonBody(String result) throws JsonProcessingException, IOException {
+	  
+    mapper = new ObjectMapper();
+    ObjectNode root = (ObjectNode) mapper.readTree(result);
+    ObjectNode parentData = (ObjectNode) root.path("data");
+    ArrayNode children = (ArrayNode) parentData.path("children");
+    ObjectNode childArray = (ObjectNode) children.get(0);
+    ObjectNode childData = (ObjectNode) childArray.path("data");
+
+    Data data = mapper.readValue(childData.toString(), Data.class);
+
+    // Pretty Print JSON
+    return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+  }
 }
